6bfaf04b66f97c836b6e584d3b62cbf450624892
ClearLove443 <1127280933@qq.com>
2021-11-21 17:57:58 +0800

    add ErrorMessageModule ValidationMessagesModule

--- src/app/shared/components/shared-component.module.ts
+++ src/app/shared/components/shared-component.module.ts
@@ -1,9 +1,11 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ErrorMessageModule } from './error-message/error-message.module';
 import { G2CardModule } from './g2-card/g2-card.module';
 import { G2MiniAreaModule } from './g2-mini-area/g2-mini-area.module';
 import { G2MiniBarModule } from './g2-mini-bar/g2-mini-bar.module';
 import { G2MiniProgressModule } from './g2-mini-progress/g2-mini-progress.module';
 import { TrendModule } from './trend/trend.module';
+import { ValidationMessagesModule } from './validation-messages/validation-messages.module';
 
 const COMPONENTS_CORE = [
   G2CardModule,
@@ -11,6 +13,8 @@ const COMPONENTS_CORE = [
   G2MiniBarModule,
   G2MiniProgressModule,
   TrendModule,
+  ErrorMessageModule,
+  ValidationMessagesModule,
 ];
 
 /**
