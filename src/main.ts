f6d3dbafcbce0fa86daca258a57a649d8a57c853
ClearLove443 <1127280933@qq.com>
2021-11-21 18:04:12 +0800

    the app’s state is saving it to localStorage/sessionStorage. and restore it after a refresh.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { persistState } from '@datorama/akita';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+const storage = persistState();
+
+const providers = [{ provide: 'persistStorage', useValue: storage }];
+
+platformBrowserDynamic(providers)
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
