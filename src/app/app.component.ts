92ffacba73b3273132aec526158783ce92f61886
ClearLove443 <1127280933@qq.com>
2021-11-21 17:52:21 +0800

    set default language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public languages = ['de', 'en', 'es', 'fr', 'it', 'nl'];
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('ja');
+    this.translate.use('ja');
+  }
+}
